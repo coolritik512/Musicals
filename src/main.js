44a567605e914103499323e3cae2413b2a6fbf32
Ritik <coolritik345@gmail.com>
2023-02-01 12:59:38 +0530

    intial setup

---
+++ src/main.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded',()=>{
+
+  if(window.localStorage.getItem('accessToken')){
+    window.location.href='/dashboard/dashboard.html';
+  }
+  else {
+    window.location.href='/login/login.html';
+  }
+})
